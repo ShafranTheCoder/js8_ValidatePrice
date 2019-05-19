fe2c3935308dc7343831bc8486f56166284e2df3
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-05-19 18:58:03 +0300

    finally did js 8

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 'use strict';
 
-function showList(list) {
+document.getElementById("price").onblur = function () {
+    let tempPrice = +price.value;
+    price.innerText = tempPrice;
+    if (tempPrice < 0) {
+        document.getElementById('tip').innerText = `Please enter correct price`;
+        document.getElementById('price').classList.add('redOutline');
+    } else {
+        document.getElementById('price').classList.remove('redOutline');
+        document.getElementById('tip').innerText = ``;
+        let spanList = document.getElementById('spanList');
+        let spanNode = document.createElement('span');
+        spanNode.setAttribute('onclick', `removespan(this);`);
+        let textNode = document.createTextNode(`Текущая цена: ${tempPrice}`);
+        spanNode.appendChild(textNode);
+        spanList.appendChild(spanNode);
+        console.log(tempPrice);
+    }
+};
 
-    let newArray = list.map(function(element) {
-
-        let listNode = document.getElementById('list');
-        let liNode = document.createElement('LI');
-        let textNode = document.createTextNode(element);
-        liNode.appendChild(textNode);
-        listNode.appendChild(liNode);
-        return element;
-    });
-    console.log(newArray);
-    let i = 10;
-    let timerId = setInterval(function() {
-        document.getElementById("timer").innerText = i;
-        if (i == 0) clearInterval(timerId);
-
-        i--;
-    }, 1000);
-    newArray = [];
-}
-
-showList(['hello', 'world', {name: "Dima", sName: "Bog"}, 'Kiev', 'Kharkiv', 'Odessa', 'Lviv']);
-
+let removespan = function(span) {
+    span.parentNode.removeChild(span);
+};
